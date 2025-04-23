5c786edf14764a9f12d430cc7c02e86b1c251438
Артем Ващенко <ipz231_vai@student.ztu.edu.ua>
2025-04-24 00:20:30 +0300

    Lab #4 PR for Task 4 commit

---
+++ lab-3/Composite/Strategy/LightImageNode.cs
@@ -0,0 +1,34 @@
+﻿namespace Composite.Strategy
+{
+    public class LightImageNode : LightElementNode
+    {
+        public string Src { get; set; }
+        private ISourceStrategy sourceStrategy;
+
+        public LightImageNode(string src) : base("img", false, true)
+        {
+            Src = src;
+            sourceStrategy = GetStrategy(src);
+        }
+
+        private ISourceStrategy GetStrategy(string src)
+        {
+            return (src.StartsWith("http://") || src.StartsWith("https://"))
+                   ? new WebSourceStrategy()
+                   : new FileSourceStrategy();
+        }
+
+        public void LoadImage()
+        {
+            sourceStrategy.Load(Src);
+        }
+
+        public override string OuterHtml
+        {
+            get
+            {
+                return $"<{TagName} src='{Src}' />";
+            }
+        }
+    }
+}
